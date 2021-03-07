2bd0311f06bd7975cd87201681e9966ef8213d13
Tonmoy Malik <tonmoy@rudderstack.com>
2021-03-07 12:33:52 +0530

    update prod environments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  whitelist_domain: 'poc-angular-app.azurewebsites.net',
+  base_url: 'hhttps://poc-angular-app.azurewebsites.net',
+  endpoint: "https://poc-node-api.azurewebsites.net/api/v1/",
+  secret_key: 'b*8~+j>JzYu_6e;j'
 };
