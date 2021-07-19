480dd873e8985e656a23a0e91236d5a731d4331f
Tonmoy Malik <tonmoy.malik@gmail.com>
2021-07-19 18:35:12 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   whitelist_domain: 'poc-angular-app.azurewebsites.net',
   base_url: 'hhttps://poc-angular-app.azurewebsites.net',
-  endpoint: "https://poc-node-api.azurewebsites.net/api/v1/",
+  endpoint: "http://app-0e712f87-06e5-461e-8f7c-81a0651a166f.cleverapps.io/api/v1/",
   secret_key: 'b*8~+j>JzYu_6e;j'
 };
