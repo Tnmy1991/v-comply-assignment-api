2c73de947696c2106c7b4098978493a00a913d18
Tonmoy Malik <tonmoy.malik@gmail.com>
2021-07-20 14:28:23 +0530

    api endpoint updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   whitelist_domain: 'tnmy1991.gihub.io',
   base_url: 'https://tnmy1991.gihub.io',
-  endpoint: "http://app-0e712f87-06e5-461e-8f7c-81a0651a166f.cleverapps.io/api/v1/",
+  endpoint: "https://app-0e712f87-06e5-461e-8f7c-81a0651a166f.cleverapps.io/api/v1/",
   secret_key: 'b*8~+j>JzYu_6e;j'
 };
