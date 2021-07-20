0ed4de4b5f356854f6f0115bfc27b175b140896d
Tonmoy Malik <tonmoy.malik@gmail.com>
2021-07-20 09:34:59 +0530

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  whitelist_domain: 'poc-angular-app.azurewebsites.net',
-  base_url: 'hhttps://poc-angular-app.azurewebsites.net',
+  whitelist_domain: 'tnmy1991.gihub.io',
+  base_url: 'https://tnmy1991.gihub.io',
   endpoint: "http://app-0e712f87-06e5-461e-8f7c-81a0651a166f.cleverapps.io/api/v1/",
   secret_key: 'b*8~+j>JzYu_6e;j'
 };
